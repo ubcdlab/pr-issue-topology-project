2668c2c32e9c55356a82cb77c5cf0e676f85eadd
Frozemint <nami5504@gmail.com>
2022-05-30 12:17:44 -0700

    ffs

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import json
 
 TARGET_REPO = 'jekyll/jekyll-admin'
+REGEX_STRING = '(?:\/jekyll\/jekyll-admin\/)(?:issues|pull)+\/(\d+)'
 
 def get_token():
     # get personal access token
@@ -21,8 +22,15 @@ def get_highest_issue_number(repo):
     latest_issue = list(repo.get_issues(state='all', since=datetime.datetime(2022, 5, 1), direction='desc'))
     return latest_issue[0].number
 
-def remove_hashtag(regex_findall):
-    return list(map(lambda x: x[1:], regex_findall))
+def rematch(string):
+    result = re.search('(\d)+', string)
+    # print(result.start())
+    return result.start()
+
+def extract_number(regex_findall):
+    result = list(map(rematch, regex_findall))
+    print(f"Extracted: {result}")
+    return result
 
 
 g = Github(get_token())
@@ -33,17 +41,21 @@ for issue_number in range(1, get_highest_issue_number(repo)):
     try:
         item = repo.get_issue(issue_number)
         total_links = []
-        regex_matches = re.findall('#[0-9]+', item.body)
+        regex_matches = [x.group(1) for x in re.finditer(REGEX_STRING, item.body)]
+
         if (item.user.type != 'Bot'):
-            total_links = total_links + remove_hashtag(regex_matches)
+            total_links = total_links + regex_matches
+
         for comment in item.get_comments():
-            regex_matches = re.findall('#[0-9]+', comment.body)
+            regex_matches = [x.group(1) for x in re.finditer(REGEX_STRING, comment.body)]
+            regex_matches = regex_matches + re.findall(f'(?<=#){issue_number}', comment.body) # find self-loops
             if (comment.user.type != 'Bot'):
-                total_links = total_links + remove_hashtag(regex_matches)
+                total_links = total_links + regex_matches
         graph_dict.update({issue_number: total_links})
+        print({issue_number: total_links})
 
     except Exception as e:
-        print(e)
+        print('')
 
 with open('graph.txt', 'w') as f:
     f.write(json.dumps(graph_dict, sort_keys=True, indent=4))
---
+++ vis.py
@@ -0,0 +1,19 @@
+import json 
+
+f = open('graph.txt')
+data = json.load(f)
+
+vis_data = {
+	"nodes": [],
+	"links": []
+}
+
+for key, value in data.items():
+	node = {"id": int(key), "name": key}
+	vis_data['nodes'].append(node)
+	for destination in value:
+		linking = {"source": int(key), "target": int(destination)}
+		vis_data['links'].append(linking)
+
+with open('vis_graph.json', 'w') as f:
+	f.write(json.dumps(vis_data, sort_keys=True, indent=4))
