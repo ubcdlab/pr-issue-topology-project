5431a0b6e0fd05000ddb4bd3e3da576575fd49bb
Frozemint <nami5504@gmail.com>
2022-05-30 21:00:21 -0700

    dragging that kind of work

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@
 
 
 d3.json('data/vis_graph.json').then(data => {
+  for (let node of data.nodes) {
+    node['isolated'] = true;
+    for (let link of data.links) {
+      // console.log(link)
+      if (link.source === node.id || link.target === node.id) {
+        node['isolated'] = false;
+      }
+    }
+  }
   console.log(data);
+
   const networkplot = new Networkvis({});
   networkplot.updateVis(data);
 })
--- js/networkvis.js
+++ js/networkvis.js
@@ -1,22 +1,58 @@
 class Networkvis {
     constructor() {
         this.config = {
-            width: 900,
-            height: 900,
+            width: 1500,
+            height: 1000,
         }
+        this.data = null;
         this.initVis();
     }
     initVis() {
         let vis = this;
-        vis.svg = d3.select('#vis')
-        .append('svg')
-        .attr('width', vis.config.width)
-        .attr('height', vis.config.height);
+        vis.hideIsolatedNodes = d3.select('#hideIsolatedNodes')
+        .on('change', vis.update);
+
     }
+    update() {
+        d3.selectAll('.isolated')
+        .attr('opacity', d3.select('#hideIsolatedNodes').property('checked') ? 0.1 : 1)
+    }
+
     updateVis(data) {
         let vis = this;
         vis.renderVis(data);
     }
+
+    drag_start(d, e) {
+        let vis = this;
+        //  if (!e.active)  {
+        //     vis.simulation.alphaTarget(0.3).restart()
+        // }
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    //make sure you can't drag the circle outside the box
+    drag_drag(d, e) {
+      d.fx = e.x;
+      d.fy = e.y;
+    }
+
+    drag_end(d, e) {
+        let vis = this;
+    //   if (!e.active)  {
+    //     vis.simulation.alphaTarget(0);
+    // }
+      d.fx = null;
+      d.fy = null;
+    }
+
+    //Zoom functions 
+    zoom_actions(e) {
+        let vis = this;
+        d3.select('#vis').attr("transform", e.transform)
+    }
+
     renderVis(data) {
         function ticked() {
             links
@@ -31,6 +67,11 @@ class Networkvis {
         }
 
         let vis = this;
+
+        vis.svg = d3.select('#vis')
+        .attr('width', vis.config.width)
+        .attr('height', vis.config.height);
+
         let div = d3.select('body').append('div')   
         .attr('class', 'tooltip')               
         .style('opacity', 0);
@@ -45,6 +86,8 @@ class Networkvis {
         .selectAll('circle')
         .data(data.nodes)
         .join('circle')
+        .attr('class', 'circle')
+        .classed('isolated', d => (d.isolated))
         .attr('r', 5)
         .style('fill', '#69b3a2')
         .on('mouseover', (event, d) => {
@@ -62,14 +105,27 @@ class Networkvis {
         })
 
 
-        const simulation = d3.forceSimulation(data.nodes)
+        vis.simulation = d3.forceSimulation(data.nodes)
         .force('link', d3.forceLink()
             .id(function(d) { return d.id; })
             .links(data.links))
-        .force('charge', d3.forceManyBody().strength(-1))
+        .force('charge', d3.forceManyBody().strength(-2))
         .force('center', d3.forceCenter(vis.config.width / 2, vis.config.height / 2))
-        .on('end', ticked);
+        .on("tick", ticked );
 
+        var drag_handler = d3.drag()
+        .on("start", vis.drag_start)
+        .on("drag", vis.drag_drag)
+        .on("end", vis.drag_end);   
+        
+        drag_handler(node);
+
+        //add zoom capabilities 
+        var zoom_handler = d3.zoom()
+            .on("zoom", vis.zoom_actions);
+
+        zoom_handler(vis.svg); 
     }
+
     
 }
