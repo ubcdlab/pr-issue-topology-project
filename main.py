10794284675c92f15e7b10b3d764fa6f776ab5e2
Jesse <nami5504@gmail.com>
2022-05-31 11:31:08 -0700

    added comments

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def find_all_mentions(text):
     regex_matches = []
     regex_matches += [x.group(1) for x in re.finditer(REGEX_STRING, text)]
     regex_matches += re.findall(REGEX_NUMBER_STRING, text)
-    return regex_matches
+    return regex_matches # return all matches in an array
 
 
 g = Github(get_token())
@@ -52,26 +52,35 @@ repo = g.get_repo(TARGET_REPO)
 HIGHEST_ISSUE_NUMBER = get_highest_issue_number(repo)
 
 for issue_number in range(1, HIGHEST_ISSUE_NUMBER):
+    # go through every possible issue/PR number
     try:
-        item = repo.get_issue(issue_number)
-        total_links = []
+        item = repo.get_issue(issue_number) # get the issue
+        total_links = [] # initialise array of matches, set to empty
 
-        if (item.user.type != 'Bot'):
-            total_links += find_all_mentions(item.body)
+        if (item.user.type != 'Bot'): # filter out links/mentions made by bot accounts 
+            total_links += find_all_mentions(item.body) # find all mentions in issue/PR description
 
-        for comment in item.get_comments():
-            if (comment.user.type != 'Bot'):
-                total_links += find_all_mentions(comment.body)
-        # filter out mentions to other projects
+        for comment in item.get_comments(): # go through every single comment
+            if (comment.user.type != 'Bot'): # filter out links/mentions made by bot accounts
+                total_links += find_all_mentions(comment.body) # find all mentions in a specific comment
+        
+        # filter out mentions to other projects, right now we catch mentions
+        # to other projects by removing links with an issue/PR number that is higher
+        # than the project of concern
         total_links = list(filter(lambda x: (int(x) <= HIGHEST_ISSUE_NUMBER), total_links))
 
+        # update the dictionary storing the graph toplogy 
         graph_dict.update({issue_number: total_links})
         print({issue_number: total_links})
 
     except Exception as e:
+        # Exceptions usually happens, when we try to load a number 
+        # that doesn't correspond to an issue or pull request
+        # idk why this happens
         print(e)
 
 with open('graph.txt', 'w') as f:
+    # save result to disk
     f.write(json.dumps(graph_dict, sort_keys=True, indent=4))
 
 
