b4a614a80e019acf1f8605f31a937df4e7fc2289
Frozemint <nami5504@gmail.com>
2022-05-30 17:21:58 -0700

    fix github mention detection bug

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sys
 import json
 
 TARGET_REPO = 'jekyll/jekyll-admin'
-REGEX_STRING = '(?:\/jekyll\/jekyll-admin\/)(?:issues|pull)+\/(\d+)'
 
 def get_token():
     # get personal access token
@@ -22,35 +21,30 @@ def get_highest_issue_number(repo):
     latest_issue = list(repo.get_issues(state='all', since=datetime.datetime(2022, 5, 1), direction='desc'))
     return latest_issue[0].number
 
-def rematch(string):
-    result = re.search('(\d)+', string)
-    # print(result.start())
-    return result.start()
-
-def extract_number(regex_findall):
-    result = list(map(rematch, regex_findall))
-    print(f"Extracted: {result}")
-    return result
+def find_all_mentions(text):
+    REGEX_STRING = '(?:\/jekyll\/jekyll-admin\/)(?:issues|pull)+\/(\d+)'
+    REGEX_NUMBER_STRING = '(?<=#)\d+'
+    regex_matches = []
+    regex_matches += [x.group(1) for x in re.finditer(REGEX_STRING, text)]
+    regex_matches += re.findall(REGEX_NUMBER_STRING, text)
+    return regex_matches
 
 
 g = Github(get_token())
 graph_dict = {}
 repo = g.get_repo(TARGET_REPO)
 
-for issue_number in range(1, get_highest_issue_number(repo)):
+for issue_number in range(199, get_highest_issue_number(repo)):
     try:
         item = repo.get_issue(issue_number)
         total_links = []
-        regex_matches = [x.group(1) for x in re.finditer(REGEX_STRING, item.body)]
 
         if (item.user.type != 'Bot'):
-            total_links = total_links + regex_matches
+            total_links += find_all_mentions(item.body)
 
         for comment in item.get_comments():
-            regex_matches = [x.group(1) for x in re.finditer(REGEX_STRING, comment.body)]
-            regex_matches = regex_matches + re.findall(f'(?<=#){issue_number}', comment.body) # find self-loops
             if (comment.user.type != 'Bot'):
-                total_links = total_links + regex_matches
+                total_links += find_all_mentions(comment.body)
         graph_dict.update({issue_number: total_links})
         print({issue_number: total_links})
 
