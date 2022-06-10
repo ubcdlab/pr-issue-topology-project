60ea311f8c81786d8c1b361ec903fa0b5249fcfb
Frozemint <nami5504@gmail.com>
2022-06-10 13:49:59 -0700

    add automated analysis

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
-import json 
+import json
+import sys 
 
 f = open('data/graph.json')
 data = json.load(f)
@@ -9,12 +10,15 @@ duo_issue_issue = []
 duo_issue_pr = []
 duo_pr_pr = []
 duo_pr_issue = []
+counter = 0
 
 def find_node(node_id):
 	for node in data['nodes']:
 		if node['id'] == node_id:
 			return node
 
+def compute_node_order(source_node, sink_node):
+	return [source_node['type'], sink_node['type']]
 
 # First, find isolated nodes
 for component in data['connected_components']:
@@ -29,29 +33,43 @@ id, type, status, links, connected_component, node_degree
 '''
 
 # Second, find all possible permutations of duo nodes
-for node in data['nodes']:
-	if len(node['connected_component']) != 2 or len(node['links']) == 0:
-		# ignore sink nodes 
+for component in data['connected_components']:
+	if len(component) != 2:
 		continue
-	current_node = node
-	other_node = None
-	for constituent_id in node['connected_component']:
-		if constituent_id == node:
-			continue
-		other_node = find_node(constituent_id)
-	# Today on mythconfirms, what happens when write bad code?
-	if node['type'] == 'issue' and other_node['type'] == 'issue':
-		duo_issue_issue.append([node, other_node])
-	elif node['type'] == 'issue' and other_node['type'] == 'pull_request':
-		duo_issue_pr.append([node, other_node])
-	elif node['type'] == 'pull_request' and other_node['type'] == 'pull_request':
-		duo_pr_pr.append([node, other_node])
-	elif node['type'] == 'pull_request' and other_node['type'] == 'issue':
-		duo_pr_issue.append([node, other_node])
+	counter += 1
+	component.sort()
+	lower_number_node = find_node(component[0])
+	higher_number_node = find_node(component[1])
+
+	source_node = None
+	sink_node = None
+
+	if len(lower_number_node['links']) == 0:
+		sink_node = lower_number_node
+		source_node = higher_number_node
+	else:
+		sink_node = higher_number_node
+		source_node = lower_number_node
+
+	ordering = compute_node_order(source_node, sink_node)
+	if ordering == ['issue', 'issue']:
+		duo_issue_issue.append([source_node, sink_node])
+	elif ordering == ['issue', 'pull_request']:
+		duo_issue_pr.append([source_node, sink_node])
+	elif ordering == ['pull_request', 'pull_request']:
+		duo_pr_pr.append([source_node, sink_node])
+	elif ordering == ['pull_request', 'issue']:
+		duo_pr_issue.append([source_node, sink_node])
 
 pattern_json['duo_issue_issue'] = duo_issue_issue
 pattern_json['duo_issue_pr'] = duo_issue_pr
 pattern_json['duo_pr_pr'] = duo_pr_pr
 pattern_json['duo_pr_issue'] = duo_pr_issue
 
-print(pattern_json)
+print(len(duo_issue_issue) + len(duo_issue_pr) + len(duo_pr_pr) + len(duo_pr_issue))
+print(counter)
+
+with open('data/structure.json', 'w') as f:
+	f.write(json.dumps(pattern_json, sort_keys=False, indent=4))
+
+# print(pattern_json)
