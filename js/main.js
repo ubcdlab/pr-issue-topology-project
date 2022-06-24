85e9feea79af91e9fda89ec96a3970493d0c1698
Frozemint <nami5504@gmail.com>
2022-06-24 14:35:23 -0700

    refactoring files to be truly modular

--- js/main.js
+++ js/main.js
@@ -6,9 +6,12 @@ const RIGHT_VIS_DIV_ID = '#rightVis'
 const RIGHT_VIS_DIV_ID_STRING = LEFT_VIS_DIV_ID.substring(1)
 
 
-Promise.all([
-  d3.json('data/graph_jekyll-admin.json'),
-  d3.json('data/structure_jekyll-admin.json')
+this.createVisInstance('#leftVis', 'data/graph_jekyll-admin.json', 'data/structure_jekyll-admin.json');
+
+function createVisInstance(DIV_ID, graph_json_file, structure_json_file) {
+  Promise.all([
+  d3.json(graph_json_file),
+  d3.json(structure_json_file)
 ])
 .then(data => {
   let graph_data = data[0];
@@ -17,7 +20,7 @@ Promise.all([
   console.log(graph_data);
 
   const default_slider_value = [-Infinity, Infinity]
-  let sliderDiv = d3.select(LEFT_VIS_DIV_ID)
+  let sliderDiv = d3.select(DIV_ID)
   .append('svg')
   .attr('class', 'sliderDiv')
   .style('width', '100%')
@@ -36,7 +39,7 @@ Promise.all([
   .on('end', (val) => {
       let modify = filterNetwork(val[0], val[1], graph_data)
       d3.select('#leftVis-view').remove();
-      const networkplot2 = new Networkvis(modify, LEFT_VIS_DIV_ID);
+      const networkplot2 = new Networkvis(modify, DIV_ID);
       networkplot2.updateVis(modify);
       computeStatistics(graph_data, modify)
   });
@@ -52,77 +55,21 @@ Promise.all([
   .call(slider);
 
   let modify = filterNetwork(-Infinity, Infinity, graph_data);
-  const networkplot2 = new Networkvis(modify, LEFT_VIS_DIV_ID);
+  const networkplot2 = new Networkvis(modify, DIV_ID);
   networkplot2.updateVis(modify);
 
   computeStatistics(graph_data, modify);
   
-  const patternplot = new Patternvis(structure_data, LEFT_VIS_DIV_ID);
+  const patternplot = new Patternvis(structure_data, DIV_ID);
   patternplot.updateVis(structure_data);
 
 })
 .catch(error => {
   console.log(error);
 })
+}
 
-Promise.all([
-  d3.json('data/graph_Notepads.json'),
-  d3.json('data/structure_Notepads.json')
-])
-.then(data => {
-  let graph_data = data[0];
-  let structure_data = data[1];
 
-  console.log(graph_data);
-
-  const default_slider_value = [-Infinity, Infinity]
-  let sliderDiv = d3.select(RIGHT_VIS_DIV_ID)
-  .append('svg')
-  .attr('class', 'sliderDiv')
-  .style('width', '100%')
-  .style('height', '80px');
-
-  let slider = d3.sliderBottom()
-  .min(1)
-  .max(computeLargestConnectedComponentSize(graph_data))
-  .step(1)
-  .displayValue(true)
-  .width(400)
-  .height(10)
-  .ticks(10)
-  .default(default_slider_value)
-  .fill('skyblue')
-  .on('end', (val) => {
-      let modify = filterNetwork(val[0], val[1], graph_data)
-      d3.select('#leftVis-view').remove();
-      const networkplot2 = new Networkvis(modify, RIGHT_VIS_DIV_ID);
-      networkplot2.updateVis(modify);
-      computeStatistics(graph_data, modify)
-  });
-
-  d3.select('.sliderDiv')
-  .append('svg')
-  .attr('class', 'slider')
-  .attr('id', `${RIGHT_VIS_DIV_ID_STRING}-slider-div`)
-  .attr('width', '100%')
-  .attr('height', 80)
-  .append('g')
-  .attr('transform', 'translate(30,30)')
-  .call(slider);
-
-  let modify = filterNetwork(-Infinity, Infinity, graph_data);
-  const networkplot2 = new Networkvis(modify, RIGHT_VIS_DIV_ID);
-  networkplot2.updateVis(modify);
-
-  computeStatistics(graph_data, modify);
-  
-  const patternplot = new Patternvis(structure_data, RIGHT_VIS_DIV_ID);
-  patternplot.updateVis(structure_data);
-
-})
-.catch(error => {
-  console.log(error);
-})
 
 
 function filterNetwork(min_size, max_size, data) {
