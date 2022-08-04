c190d9634baa2f6421941b281bb80a7a0fe063dc
Frozemint <nami5504@gmail.com>
2022-08-04 11:05:58 -0700

    add first temporal analysis

--- graph_analysis.py
+++ graph_analysis.py
@@ -53,7 +53,7 @@ def calculate_fixes_relationship_component_percentage(graph, TARGET_REPO_FILE_NA
             if edge_type == 'fixes':
                 fixes_count += 1
         csv_row_entry = [index, 
-                        (fixes_count / component_edge_count) * 100, # prevent division by 0 
+                        (fixes_count / component_edge_count) * 100,
                         fixes_count,  
                         component_edge_count,
                         component]
@@ -119,8 +119,9 @@ def calculate_summary(graph, TARGET_REPO_FILE_NAME, csv_rows):
         csv_rows.append(csv_row_entry)
     return csv_rows
 
-def calculate_work_done_before_merge(graph):
-    csv_column_header = ['repo_name' ,'component_size', 'component_frequency', 'edge_count', 'max_possible', 'subgraph_density']
+def calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME):
+    csv_column_header = ['repo_name', 'component_number', 'component_size', 'preexisting_nodes', 'preexisting_node_count', 'percentage', 'last_merged_node']
+    csv_rows = []
     undirected_graph = graph.to_undirected()
     connected_components = list(nx.connected_components(undirected_graph))
     for index, component in enumerate(connected_components):
@@ -128,19 +129,32 @@ def calculate_work_done_before_merge(graph):
             continue
         component_subgraph = undirected_graph.subgraph(component)
         merged_nodes = list(filter(lambda d: d[1]['status'] == 'merged', component_subgraph.nodes(data=True)))
+        if len(merged_nodes) < 1:
+            continue
         most_recent_merged_node = merged_nodes[-1]
         for node_number, node_attribute in merged_nodes:
             if node_attribute['closed_at'] > most_recent_merged_node[1]['closed_at']:
                 most_recent_merged_node = (node_number, node_attribute)
         print('')
-        # for node in component_subgraph.nodes(data=True):
-        #     node_is_merged = True if graph.nodes[node]['status'] == 'merged' else False
-        #     if node_is_merged is not True:
-        #         continue
-            
-            
-    return
+        # Having found the most recent merged node, find out how much of the connected
+        # component existed before the node was merged
+        preexisting_nodes = []
+        for node in component_subgraph.nodes(data=True):
+            if node == most_recent_merged_node:
+                continue
+            if node[1]['created_at'] < most_recent_merged_node[1]['closed_at']:
+                preexisting_nodes.append(node)
 
+        csv_row_entry = [TARGET_REPO_FILE_NAME,
+                        index,
+                        len(component),
+                        list(map(lambda x: x[0], preexisting_nodes)),
+                        len(preexisting_nodes),
+                        len(preexisting_nodes) / len(component),
+                        most_recent_merged_node[0]]
+        csv_rows.append(csv_row_entry)
+    return csv_column_header, csv_rows
+            
 def main():
     TARGET_REPO_ARRAY = sys.argv[1:]
     csv_rows = []
@@ -151,7 +165,8 @@ def main():
         graph = construct_graph(graph_json)
 
         csv_rows = calculate_summary(graph, TARGET_REPO_FILE_NAME, csv_rows)
-        calculate_work_done_before_merge(graph)
+        csv_merge_column_header, csv_merge_rows = calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME)
+        write_csv_to_file(csv_merge_column_header, csv_merge_rows, TARGET_REPO_FILE_NAME, 'merge')
 
     # write_csv_to_file(csv_column_header, csv_rows, 'csv_summary', '')
 
