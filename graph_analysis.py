5b1d75507956ba92e4cfc7ede8b6c25e8c7f3d51
Frozemint <nami5504@gmail.com>
2022-08-08 09:35:45 -0700

    modified output stats slightly

--- graph_analysis.py
+++ graph_analysis.py
@@ -120,7 +120,7 @@ def calculate_summary(graph, TARGET_REPO_FILE_NAME, csv_rows):
     return csv_rows
 
 def calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME, csv_rows):
-    csv_column_header = ['repo_name', 'component_number', 'component_size', 'component_nodes', 'preexisting_nodes', 'preexisting_node_count', 'percentage', 'last_merged_node']
+    csv_column_header = ['repo_name', 'component_number', 'component_size', 'component_nodes', 'preexisting_nodes', 'preexisting_node_count', 'percentage', 'last_merged_node', 'merged_node_count']
     undirected_graph = graph.to_undirected()
     connected_components = list(nx.connected_components(undirected_graph))
     for index, component in enumerate(connected_components):
@@ -136,21 +136,22 @@ def calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME, csv_rows):
                 most_recent_merged_node = (node_number, node_attribute)
         # Having found the most recent merged node, find out how much of the connected
         # component existed before the node was merged
-        preexisting_nodes = []
+        post_merge_nodes = []
         for node in component_subgraph.nodes(data=True):
             if node == most_recent_merged_node:
                 continue
-            if node[1]['created_at'] < most_recent_merged_node[1]['closed_at']:
-                preexisting_nodes.append(node)
+            if node[1]['created_at'] > most_recent_merged_node[1]['closed_at']:
+                post_merge_nodes.append(node)
 
         csv_row_entry = [TARGET_REPO_FILE_NAME,
                         index,
                         len(component),
                         component,
-                        list(map(lambda x: x[0], preexisting_nodes)),
-                        len(preexisting_nodes),
-                        len(preexisting_nodes) / len(component),
-                        most_recent_merged_node[0]]
+                        list(map(lambda x: x[0], post_merge_nodes)),
+                        len(post_merge_nodes),
+                        len(post_merge_nodes) / len(component),
+                        most_recent_merged_node[0],
+                        len(merged_nodes)]
         csv_rows.append(csv_row_entry)
     return csv_column_header, csv_rows
             
