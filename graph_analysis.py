f4a6f438493aa407a129712ac0cdff011366d387
Frozemint <nami5504@gmail.com>
2022-08-11 14:43:32 -0700

    rework existing analysis functions slightly

--- graph_analysis.py
+++ graph_analysis.py
@@ -1,9 +1,7 @@
 import json
 import sys 
-import pickle
 import networkx as nx
 import csv
-from os.path import exists
 
 def read_json_from_file(TARGET_REPO_FILE_NAME):
     PATH = f'data/graph_{TARGET_REPO_FILE_NAME}.json'
@@ -41,11 +39,11 @@ def max_number_possible_edges_directed(graph):
 def max_number_possible_edges_directed_nodenum(n):
     return (n * (n - 1))
 
-def calculate_fixes_relationship_component_percentage(graph, TARGET_REPO_FILE_NAME):
+def calculate_fixes_relationship_component_percentage(graph, TARGET_REPO_FILE_NAME, csv_rows):
     undirected_graph = graph.to_undirected()
-    csv_column_header = ['component', 'percentage', 'fixes_count', 'edge_count', 'component_nodes']
-    csv_rows = []
+    csv_column_header = ['key_field', 'repo_name', 'component_number', 'component_size', 'percentage', 'fixes_count', 'edge_count', 'component_nodes']
     connected_components = list(nx.connected_components(undirected_graph))
+    counter = 0
     for index, component in enumerate(connected_components):
         component_subgraph = graph.subgraph(component)
         component_edge_count = component_subgraph.number_of_edges()
@@ -57,12 +55,16 @@ def calculate_fixes_relationship_component_percentage(graph, TARGET_REPO_FILE_NA
             edge_type = edge[2]['type']
             if edge_type == 'fixes':
                 fixes_count += 1
-        csv_row_entry = [index, 
-                        (fixes_count / component_edge_count) * 100,
+        csv_row_entry = [f'{TARGET_REPO_FILE_NAME}+{counter}',
+                        TARGET_REPO_FILE_NAME,
+                        index,
+                        len(component), 
+                        (fixes_count / component_edge_count),
                         fixes_count,  
                         component_edge_count,
                         component]
         csv_rows.append(csv_row_entry)
+        counter += 1
     return csv_column_header, csv_rows
 
 def calculate_connected_component_density(graph):
@@ -108,11 +110,12 @@ def calculate_summary(graph, TARGET_REPO_FILE_NAME, csv_rows):
     return csv_column_header, csv_rows
 
 def calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME, csv_rows):
-    csv_column_header = ['repo_name', 'component_number', 'component_size', 'component_nodes', 'preexisting_nodes', 'preexisting_node_count', 'percentage', 'last_merged_node', 'merged_node_count']
+    csv_column_header = ['key_field', 'repo_name', 'component_number', 'component_size', 'component_nodes', 'preexisting_nodes', 'preexisting_node_count', 'percentage', 'last_merged_node', 'merged_node_count']
     undirected_graph = graph.to_undirected()
     connected_components = list(nx.connected_components(undirected_graph))
+    counter = 0
     for index, component in enumerate(connected_components):
-        if len(component) <= 2:
+        if len(component) <= 1:
             continue
         component_subgraph = undirected_graph.subgraph(component)
         merged_nodes = list(filter(lambda d: d[1]['status'] == 'merged', component_subgraph.nodes(data=True)))
@@ -131,7 +134,8 @@ def calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME, csv_rows):
             if node[1]['created_at'] > most_recent_merged_node[1]['closed_at']:
                 post_merge_nodes.append(node)
 
-        csv_row_entry = [TARGET_REPO_FILE_NAME,
+        csv_row_entry = [f'{TARGET_REPO_FILE_NAME}+{counter}',
+                        TARGET_REPO_FILE_NAME,
                         index,
                         len(component),
                         component,
@@ -141,6 +145,7 @@ def calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME, csv_rows):
                         most_recent_merged_node[0],
                         len(merged_nodes)]
         csv_rows.append(csv_row_entry)
+        counter += 1
     return csv_column_header, csv_rows
 
 def calculate_mean_comments_per_component_before_after_merge(graph, TARGET_REPO_FILE_NAME, csv_rows):
@@ -178,22 +183,24 @@ def calculate_mean_comments_per_node(graph, TARGET_REPO_FILE_NAME, csv_rows):
     return
 
 def calculate_diameter_per_component(graph, TARGET_REPO_FILE_NAME, csv_rows):
-    csv_column_header = ['repo_name', 'component_number', 'component_size', 'component_nodes', 'component_diameter']
+    csv_column_header = ['key_field', 'repo_name', 'component_number', 'component_size', 'component_nodes', 'component_diameter']
     undirected_graph = graph.to_undirected()
     connected_components = list(nx.connected_components(undirected_graph))
+    counter = 0
     for index, component in enumerate(connected_components):
         component_subgraph = undirected_graph.subgraph(component)
         diameter = nx.diameter(component_subgraph)
-        csv_row_entry = [TARGET_REPO_FILE_NAME,
+        csv_row_entry = [f'{TARGET_REPO_FILE_NAME}+{counter}',
+                        TARGET_REPO_FILE_NAME,
                         index,
                         len(component),
                         component,
                         diameter]
         csv_rows.append(csv_row_entry)
         print(f'Loaded component {component} in {TARGET_REPO_FILE_NAME}')
+        counter += 1
     return csv_column_header, csv_rows
     
-
 def calculate_mean_authors_per_component_before_after_merge(graph, TARGET_REPO_FILE_NAME, csv_rows):
     csv_column_header = ['repo_name', 'component_number', 'component_size', 'component_nodes', 'authors_count', 'authors_count_after_merge', 'last_merged_node']
     undirected_graph = graph.to_undirected()
@@ -227,6 +234,21 @@ def calculate_mean_authors_per_component_before_after_merge(graph, TARGET_REPO_F
         csv_rows.append(csv_row_entry)
     return csv_column_header, csv_rows
 
+def calculate_graph_connected_component_sizes_distribution(graph, TARGET_REPO_FILE_NAME, csv_rows):
+    csv_column_header = ['key_field', 'repo_name', 'component_number', 'component_size', 'component_nodes']
+    undirected_graph = graph.to_undirected()
+    connected_components = list(nx.connected_components(undirected_graph))
+    counter = 0
+    for index, component in enumerate(connected_components):
+        csv_row_entry = [f'{TARGET_REPO_FILE_NAME}+{counter}',
+                        TARGET_REPO_FILE_NAME,
+                        index,
+                        len(component),
+                        component]
+        csv_rows.append(csv_row_entry)
+        counter += 1
+    return csv_column_header, csv_rows
+
 def main():
     TARGET_REPO_ARRAY = sys.argv[1:]
     csv_rows = []
@@ -235,16 +257,20 @@ def main():
         graph_json = read_json_from_file(TARGET_REPO_FILE_NAME)
         graph = construct_graph(graph_json)
 
-        csv_column_header, csv_rows = calculate_summary(graph, TARGET_REPO_FILE_NAME, csv_rows)
-        # csv_merge_column_header, csv_merge_rows = calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME, csv_merge_rows)
+        # csv_column_header, csv_rows = calculate_summary(graph, TARGET_REPO_FILE_NAME, csv_rows)
+        csv_merge_column_header, csv_merge_rows = calculate_work_done_before_merge(graph, TARGET_REPO_FILE_NAME, csv_rows)
         # csv_author_column_header, csv_author_rows = calculate_mean_authors_per_component(graph, TARGET_REPO_FILE_NAME, csv_rows)
         # csv_comment_column_header, csv_comment_rows = calculate_mean_comments_per_component(graph, TARGET_REPO_FILE_NAME, csv_rows)
         # csv_diameter_column_header, csv_diameter_rows = calculate_diameter_per_component(graph, TARGET_REPO_FILE_NAME, csv_rows)
-    
-    write_csv_to_file(csv_column_header, csv_rows, 'density2', '')
-    # write_csv_to_file(csv_merge_column_header, csv_merge_rows, 'csv_summary', 'merge')
+        # csv_component_size_column_header, csv_rows = calculate_graph_connected_component_sizes_distribution(graph, TARGET_REPO_FILE_NAME, csv_rows)
+        # csv_fixes_column_header, csv_fixes_rows = calculate_fixes_relationship_component_percentage(graph, TARGET_REPO_FILE_NAME, csv_rows)
+
+    # write_csv_to_file(csv_column_header, csv_rows, 'density2', '')
+    write_csv_to_file(csv_merge_column_header, csv_merge_rows, 'work_done_after_merge', '')
     # write_csv_to_file(csv_column_header, csv_rows, 'csv_component_size_distribution', '')
     # write_csv_to_file(csv_diameter_column_header, csv_diameter_rows, 'csv_component_diameter', '')
+    # write_csv_to_file(csv_component_size_column_header, csv_rows, 'component_size_dist', '')
+    # write_csv_to_file(csv_fixes_column_header, csv_fixes_rows, 'fixes', '')
 
 
 if __name__ == '__main__':
