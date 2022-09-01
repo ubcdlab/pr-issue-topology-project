4686d30bce64f30c742654f4f6310ef717fa888e
Frozemint <nami5504@gmail.com>
2022-09-01 14:38:22 -0700

    update code to include more information

--- consolidate_all_repos.py
+++ consolidate_all_repos.py
@@ -2,6 +2,7 @@ import csv
 import json
 import sys
 import networkx as nx
+import re
 
 def read_json_from_file(TARGET_REPO_FILE_NAME):
     PATH = f'data/graph_{TARGET_REPO_FILE_NAME}.json'
@@ -11,9 +12,19 @@ def read_json_from_file(TARGET_REPO_FILE_NAME):
     return data
 
 def construct_graph(graph_json, TARGET_REPO):
+    # To add more data into the output file
+    # you probably want to change this function, which
+    # adds data to nodes (so you can include more info in
+    # connected components later)
     graph = nx.Graph()
     for node in graph_json['nodes']:
-        graph.add_node(node['id'])
+        authors = set()
+        for event in node['event_list']:
+            event_author = event['author']
+            if event_author == 'None' or not event_author.startswith('https://github.com'):
+                continue
+            authors.add(event_author)
+        graph.add_node(node['id'], authors=list(authors))
     for edge in graph_json['links']:
         graph.add_edge(edge['source'], edge['target'], type=edge['link_type'])
     return graph
@@ -36,28 +47,30 @@ def main():
     TARGET_REPO_ARRAY = sys.argv[1:]
     components = []
     csv_rows = []
-    csv_column_header = ['key', 'repo_name', 'size', 'diameter', 'density']
+    csv_column_header = ['key', 'repo_name', 'size', 'diameter', 'density', 'authors']
     magic_counter = 0
     for TARGET_REPO in TARGET_REPO_ARRAY:
         TARGET_REPO_FILE_NAME = TARGET_REPO.replace('/', '-')
         graph_json = read_json_from_file(TARGET_REPO_FILE_NAME)
         graph = construct_graph(graph_json, TARGET_REPO)
         connected_components = nx.connected_components(graph)
-        for index, component in enumerate(connected_components):
+        for component in connected_components:
             if len(component) <= 1:
                 continue
             subgraph = graph.subgraph(component)
             edges_in_subgraph = graph.subgraph(component).number_of_edges()
             max_possible_edges_directed = max_number_possible_edges_directed_nodenum(len(component))
             list_of_nodes = list(subgraph.nodes(data=True))
-            # components.append({
-            #     'key': magic_counter,
-            #     'repo_name': TARGET_REPO,
-            #     'nodes': list(component),
-            #     'diameter': nx.diameter(subgraph),
-            #     'density': edges_in_subgraph / max(max_possible_edges_directed, 1)
-            # })
-            csv_rows.append([magic_counter, TARGET_REPO, len(component), nx.diameter(subgraph), edges_in_subgraph / max(max_possible_edges_directed, 1)])
+            set_of_authors = set()
+            for node in list_of_nodes:
+                set_of_authors.update(node[1]['authors'])
+            csv_rows.append([magic_counter, 
+            TARGET_REPO,
+            len(component),
+            nx.diameter(subgraph),
+            edges_in_subgraph / max(max_possible_edges_directed, 1),
+            list(set_of_authors)
+            ])
             magic_counter += 1
     # write_json_to_file(components)
     write_csv_to_file(csv_column_header, csv_rows)
