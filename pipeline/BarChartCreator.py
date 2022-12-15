170606232c11894631862571b91cc0a90e514ccb
frozemint <nami5504@gmail.com>
2022-12-15 14:30:39 -0800

    refactor confirmed working

--- pipeline/BarChartCreator.py
+++ pipeline/BarChartCreator.py
@@ -1,10 +1,7 @@
-import datetime
-import re
-from github import Github
 import picklereader
 import networkx as nx
 import json
-import os
+from pathlib import Path
 
 class BarChartCreator(picklereader.PickleReader):
     def __init__(self, github_token, target_repo_list):
@@ -17,39 +14,100 @@ class BarChartCreator(picklereader.PickleReader):
     
     def write_json_to_file(self, graph_dict, target_repo):
         target_repo_no_slash = target_repo.replace('/', '-')
-        PATH = os.path.abspath('..')
-        with open(f'{PATH}/data/graph_{target_repo_no_slash}.json', 'w') as f:
+        PATH = Path(__file__).resolve().parents[1]
+        with open(f'{PATH}/data/structure_{target_repo_no_slash}.json', 'w') as f:
             f.write(json.dumps(graph_dict, sort_keys=False, indent=4))
-        print(f'Saved result to data/graph_{target_repo_no_slash}.json')
+        print(f'Saved result to data/structure_{target_repo_no_slash}.json')
+    
+    def find_node(self, nodes, id):
+        for node in nodes:
+            if node.number == id:
+                return node
+    
+    def find_link(self, links_dict, node_1, node_2):
+        for link in links_dict:
+            if link['source'] == node_1.number and link['target'] == node_2.number:
+                return node_1, node_2
+            elif link['target'] == node_1.number and link['source'] == node_2.number:
+                return node_2, node_1
+
+    def compute_node_order(self, source_node, sink_node):
+        source_node_type = 'pull_request' if source_node.pull_request is not None else 'issue'
+        sink_node = 'pull_request' if sink_node.pull_request is not None else 'issue'
+        return [source_node_type, sink_node]
 
     def create_vis_json_for_repo(self, target_repo):
         graph = nx.Graph()
-        analysis_dict = {}
+        pattern_json = {}
+        links_dict = []
 
         nodes, node_list, comment_list, timeline_list, review_comment_list = self.read_repo_local_file(None, target_repo)
         for index, issue in enumerate(nodes):
             graph.add_node(issue.number)
-            links_dict = []
             issue_timeline = timeline_list[-index-1]
             issue_timeline = list(filter(lambda x: x.event == 'cross-referenced' and x.source.issue.repository.full_name == target_repo, issue_timeline))
             for mention in issue_timeline:
                 # Tracks INCOMING MENTIONS
                 links_dict.append({
-                    'number': mention.source.issue.number,
+                    'source': mention.source.issue.number,
+                    'target': issue.number
                 })
-            for link in links_dict:
-                # add link to the graph
-                graph.add_edge(link['number'], issue.number)
+                graph.add_edge(mention.source.issue.number, issue.number)
         connected_components = list(nx.connected_components(graph))
-        max_component_size = 0
+        connected_component_sizes = set()
+
+        pattern_json = {
+            '1': {
+                'isolated': []
+            },
+            '2': {
+                'duo_issue_issue': [],
+                'duo_issue_pr': [],
+                'duo_pr_pr': [],
+                'duo_pr_issue': []
+            }
+        }
         for component in connected_components:
-            max_component_size = max(len(component), max_component_size)
-        for counter in range(1, max_component_size + 1):
-            analysis_dict.append({
-                counter: []
-            })
+            connected_component_sizes.add(len(component))
+        connected_component_sizes.remove(1)
+        connected_component_sizes.discard(2)
+        for size in connected_component_sizes:
+            pattern_json[str(size)] = {
+                'general': []
+            }
+        # Populate the isoated nodes list in pattern_json
         for isolated_node in list(nx.isolates(graph)):
-            analysis_dict['1'].append([isolated_node])
+            pattern_json['1']['isolated'].append([isolated_node])
+        counter = 0
+        # now populate the duo nodes list in pattern_json
+        for component in connected_components:
+            if len(component) != 2:
+                continue
+            counter += 1
+            component = list(component)
+            component.sort()
+
+            lower_number_node = self.find_node(nodes, component[0])
+            higher_number_node = self.find_node(nodes, component[1])
+
+            source_node, sink_node = self.find_link(links_dict, lower_number_node, higher_number_node)
+            ordering = self.compute_node_order(source_node, sink_node)
+            if ordering == ['issue', 'issue']:
+                pattern_json['2']['duo_issue_issue'].append([source_node.number, sink_node.number])
+            if ordering == ['issue', 'pull_request']:
+                pattern_json['2']['duo_issue_pull_request'].append([source_node.number, sink_node.number])
+            if ordering == ['pull_request', 'pull_request']:
+                pattern_json['2']['duo_pr_pr'].append([source_node.number, sink_node.number])
+            if ordering == ['pull_request', 'issue']:
+                pattern_json['2']['duo_pr_issue'].append([source_node.number, sink_node.number])
+
+        # now populate the rest
+        for component in connected_components:
+            component_size = len(component)
+            if component_size < 3:
+                continue
+            pattern_json[str(component_size)]['general'].append(list(component))
+        return pattern_json
         
         
 
