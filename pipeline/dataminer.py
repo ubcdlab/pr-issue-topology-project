8673046fd17905a29cd7de12e54e8d719df93611
frozemint <nami5504@gmail.com>
2022-12-14 17:52:47 -0800

    Code confirm working

--- pipeline/dataminer.py
+++ pipeline/dataminer.py
@@ -1,17 +1,9 @@
-from inspect import Attribute
-from xmlrpc.client import DateTime
 from github import Github
 import datetime
-import re
 import sys
-import json
-import networkx as nx
 import pickle
-from os.path import exists
-import os
 import time
-import contextlib
-from dateutil import parser
+import os
 
 class Dataminer(object):
     RATE_LIMIT_THRESHOLD = 100
@@ -22,7 +14,9 @@ class Dataminer(object):
         self.target_repo_list = target_repo_list
 
     def load_saved_progress(self, repo, target_repo):
-        PATH = f'./../raw_data/nodes_{target_repo}'
+        target_repo_no_slash = target_repo.replace('/', '-') # slashes breaks file path
+        PATH_FROM_PARENT_DIR = f'raw_data/nodes_{target_repo_no_slash}'
+        PATH = os.path.abspath(os.path.join(os.path.abspath('..'), PATH_FROM_PARENT_DIR))
         nodes, node_list, comment_list, timeline_list, review_comment_list = ([] for i in range(5))
         try:
             nodes = pickle.load(open(f'{PATH}.pk', 'rb'))
@@ -30,19 +24,26 @@ class Dataminer(object):
             comment_list = pickle.load(open(f'{PATH}_comments.pk', 'rb'))
             timeline_list = pickle.load(open(f'{PATH}_event.pk', 'rb'))
             review_comment_list = pickle.load(open(f'{PATH}_review_comments.pk', 'rb'))
-        except:
+        except FileNotFoundError:
+            print('One or more binary files missing. Redownloading repo files.')
             nodes = list(repo.get_issues(state='all', sort='created', direction='desc'))
             node_list = nodes.copy()
             review_comment_list = list(repo.get_pulls_review_comments(sort='created', direction='desc'))
             pickle.dump(review_comment_list, open(f'{PATH}_review_comments.pk', 'wb'))
+        except Exception as e:
+            print(e)
+            print('To the new RA, an exception (that is not FileNotFoundError) has happened, and it\'s not supposed to ever happen.')
+            sys.exit(1)
         return nodes, node_list, comment_list, timeline_list, review_comment_list
     
     def download_all_repo_data(self):
         for target_repo in self.target_repo_list:
-            repo = self.g.get_repo()
+            self.download_repo_data(target_repo)
     
-    def write_variables_to_file(self, nodes, node_list, comment_list, timeline_list, review_comment_list, TARGET_REPO_FILE_NAME):
-        PATH = f'raw_data/nodes_{TARGET_REPO_FILE_NAME}'
+    def write_variables_to_file(self, nodes, node_list, comment_list, timeline_list, review_comment_list, target_repo):
+        target_repo_no_slash = target_repo.replace('/', '-') # slashes breaks file path
+        PATH_FROM_PARENT_DIR = f'raw_data/nodes_{target_repo_no_slash}'
+        PATH = os.path.abspath(os.path.join(os.path.abspath('..'), PATH_FROM_PARENT_DIR))
         print('Writing raw nodes and comment data to disk...\nDO NOT INTERRUPT OR TURN OFF YOUR COMPUTER.')
         pickle.dump(nodes, open(f'{PATH}.pk', 'wb'))
         pickle.dump(node_list, open(f'{PATH}_progress.pk', 'wb'))
@@ -50,14 +51,14 @@ class Dataminer(object):
         pickle.dump(timeline_list, open(f'{PATH}_event.pk', 'wb'))
         pickle.dump(review_comment_list, open(f'{PATH}_review_comments.pk', 'wb'))
 
-    def check_rate_limit(self, rate_limit, nodes, node_list, comment_list, timeline_list, TARGET_REPO_FILE_NAME):
+    def check_rate_limit(self, rate_limit, nodes, node_list, comment_list, timeline_list, review_comment_list, target_repo):
         if rate_limit.core.remaining < self.RATE_LIMIT_THRESHOLD:
             print('Rate limit threshold reached!')
             time_remaining = rate_limit.core.reset - datetime.datetime.utcnow()
             print(f'Rate limit will reset after {time_remaining.seconds // 60} minutes {time_remaining.seconds % 60} seconds')
             print(f'Rate limit reset time: {rate_limit.core.reset}' ) # I am not going to bother figuring out printing local time 
             print(f'Sleeping for {time_remaining.seconds + self.EXTRA_RATE_LIMIT_WAIT_DURATION} seconds.')
-            self.write_variables_to_file(nodes, node_list, comment_list, timeline_list, TARGET_REPO_FILE_NAME)
+            self.write_variables_to_file(nodes, node_list, comment_list, timeline_list, review_comment_list, target_repo)
             time.sleep(time_remaining.seconds + self.EXTRA_RATE_LIMIT_WAIT_DURATION)
 
     def download_repo_data(self, target_repo):
@@ -67,7 +68,7 @@ class Dataminer(object):
         
         if (len(nodes) > 0 and len(comment_list) > 0) and len(nodes) == len(comment_list):
             # Already done, just return the results loaded from file
-            print('All nodes has already been downloaded and processed. Skipping download and loading saved local files.')
+            print('All nodes has already been downloaded and processed. Skipping download.')
             print('To force a re-download, delete the associated .pk pickle files and run the code again.')
             return nodes, comment_list, timeline_list, review_comment_list
         else:
@@ -75,7 +76,7 @@ class Dataminer(object):
             print(f'Nodes remaining to download from repo: {len(node_list)}')
             try:
                 while len(node_list) > 0:
-                    self.check_rate_limit(self.g.get_rate_limit(), nodes, node_list, comment_list, timeline_list, target_repo)
+                    self.check_rate_limit(self.g.get_rate_limit(), nodes, node_list, comment_list, timeline_list, review_comment_list, target_repo)
                     issue = node_list[-1] # get the lowest numbered issue we haven't processed
                     node_comments = issue.get_comments()
                     node_timeline = issue.get_timeline()
@@ -83,23 +84,15 @@ class Dataminer(object):
                     comment_list.append(list(node_comments))
 
                     node_list.pop() # remove the issue we just processed from the list
-                    print(f'Downloaded node {issue.number}. {len(node_list)} remaining. Rate limit: {g.rate_limiting[0]}')
+                    print(f'Downloaded node {issue.number}. {len(node_list)} remaining. Rate limit: {self.g.rate_limiting[0]}')
             except Exception as e:
                 # This should never happen
                 print('To the new RA, an exception has happened, and it\'s not supposed to ever happen.')
                 print(e)
-                self.write_variables_to_file(nodes, node_list, comment_list, timeline_list, review_comment_list, TARGET_REPO_FILE_NAME)
-                sys.exit(0) # abort the download process
-            # We made it through downloading the whole thing with no errors
-            # Save the full progress
-            self.write_variables_to_file(nodes, node_list, comment_list, timeline_list, review_comment_list, TARGET_REPO_FILE_NAME)
+                self.write_variables_to_file(nodes, node_list, comment_list, timeline_list, review_comment_list, target_repo)
+                sys.exit(1) # abort the download process
+            # We made it through downloading the whole thing with no errors, save the full progress
+            self.write_variables_to_file(nodes, node_list, comment_list, timeline_list, review_comment_list, target_repo)
             self.g.get_rate_limit() # call the API rate limit check to refresh our rate limit stat
-            print(f'Finished downloading entire repo. Rate limit: {g.rate_limiting[0]}')
+            print(f'Finished downloading entire repo. Rate limit: {self.g.rate_limiting[0]}')
             return nodes, comment_list, timeline_list # return the result
-
-
-
-
-
-
-    
--- pipeline/stage_1.py
+++ pipeline/stage_1.py
@@ -1,27 +1,33 @@
 import sys
 import dataminer
+import os
 
 def get_token():
     # get personal access token from a file named token.txt
     token = None
+    PATH = os.path.abspath('..')
     try:
-        with open('.token', 'r') as f:
+        with open(f'{PATH}/.token', 'r') as f:
             token = f.read()
             print('Github token read OK')
     except IOError:
-        pass
+        print('Failed to read Github token. Did you create a .token file in the project root directory?')
+        print('Exiting.')
+        sys.exit(1)
     return token
 
 def main():
     try:
         TARGET_REPO_ARRAY = sys.argv[1:]
+        if len(TARGET_REPO_ARRAY) < 1:
+            raise IndexError
     except IndexError:
         print(f'Expected at least 1 argument, found {len(sys.argv) - 1}')
-        print('Exiting')
+        print(f'You need to specify the names of Github repo to download as arguments.')
+        print('Exiting.')
         sys.exit(1)
-    dm = dataminer.Dataminer(get_token(), "Frozemint/pr_dev")
-    dm.download_repo_data()
-    print('hi')
+    dm = dataminer.Dataminer(get_token(), TARGET_REPO_ARRAY)
+    dm.download_all_repo_data()
 
 if __name__ == '__main__':
     main()
