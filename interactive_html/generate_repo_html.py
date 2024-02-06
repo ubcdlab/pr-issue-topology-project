b87490ed09ce2f4af6e5153f11b3f6d54f0d656e
Emilie Ma <kewbish@gmail.com>
2024-02-05 20:26:07 -0800

    Add comments from meeting today

--- interactive_html/generate_repo_html.py
+++ interactive_html/generate_repo_html.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 from os import walk
 from re import compile
 from os.path import isdir, join
@@ -20,21 +20,37 @@ def main(repo: str):
     rootdir = "interactive_html/embeddable/"
     regex = compile(f"^{repo.replace('/','-')}-\\d+.html")
 
+    counter = Counter()
     workflow_types = defaultdict(list)
+    most_recently_updated = None
+    recently_updated_time = float("inf")
     for root, dirs, files in walk(rootdir):
         for file in files:
             if regex.match(file):
+                counter.update({root: 1})
+                rel_path = "../" + join(root.replace("interactive_html/", ""), file)
+                last_updated = int(file.replace(".html", "").split("-")[-1])
+                if last_updated < recently_updated_time:
+                    recently_updated_time = last_updated
+                    most_recently_updated = rel_path
                 workflow_types[
-                    root.replace(rootdir, "").replace("_", " ").capitalize()
-                ].append(
-                    [file, "../" + join(root.replace("interactive_html/", ""), file)]
-                )
+                    root.replace(rootdir, "")
+                    .replace("_", " ")
+                    .title()
+                    .replace("Pr", "PR")
+                ].append([root.replace(rootdir, "") + "_" + str(counter.get(root))])
 
     with open("interactive_html/template.html", "r") as x:
         t = Template("\n".join(x.readlines()))
 
     with open(f"interactive_html/repos/{repo.replace('/','-')}.html", "w") as x:
-        x.write(t.render(repo=repo, workflow_types=workflow_types))
+        x.write(
+            t.render(
+                repo=repo,
+                workflow_types=workflow_types,
+                last_updated=most_recently_updated,
+            )
+        )
 
 
 if __name__ == "__main__":
